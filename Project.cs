ccb1c4f5204808f58bcd08d0e6f4df2d4fd0fccb
Depri0n <151538332+Depri0n@users.noreply.github.com>
2024-01-10 03:19:55 +0300

    projects update

---
+++ Project.cs
@@ -0,0 +1,23 @@
+﻿public class Project
+{
+    public string Title = "", Description = "";
+    public bool isShown = false;
+
+    public Project(string title)
+    {
+        Title = title;
+    }
+
+    public Project(string title, string desc)
+    {
+        Title = title;
+        Description = desc;
+    }
+
+    public Project(string title, string desc, bool show)
+    {
+        Title = title;
+        Description = desc;
+        isShown = show;
+    }
+}
